86a8bada2596768d40afe9bda838091769fa4995
Kaushal <Kaushal369@gmail.com>
2018-10-18 16:32:56 +0530

    Add new class AnyNumberTable

---
+++ JavaLogical/src/com/qa/logical/learning/AnyNumberTable.java
@@ -0,0 +1,32 @@
+package com.qa.logical.learning;
+
+import java.util.Scanner;
+
+public class AnyNumberTable {
+
+	public static void main(String[] args) {
+		 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter any number!!! ");
+		
+		int num = sc.nextInt();
+		
+		
+		if(num>=0)
+		{
+			for(int i=1;i<=10;i++)
+			{
+				System.out.println(num+"*"+i+"="+num*i);
+			}
+		}
+		
+		else
+		{
+			System.out.println("Please Enter Positive number !!!");
+			
+			System.out.println("Enter any number!!! ");
+		}
+	}
+
+}
