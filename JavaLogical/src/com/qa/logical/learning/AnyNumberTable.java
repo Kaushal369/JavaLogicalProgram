ec0af775cefc0110f7d8770a64360a4250135571
kaushal369@gmail.com <kaushal369@gmail.com>
2019-01-26 17:31:04 +0530

    update

--- JavaLogical/src/com/qa/logical/learning/AnyNumberTable.java
+++ JavaLogical/src/com/qa/logical/learning/AnyNumberTable.java
@@ -1,14 +1,7 @@
 package com.qa.logical.learning;
-<<<<<<< HEAD
 import java.util.Scanner;
 public class AnyNumberTable {
-
-=======
- import java.util.Scanner;
- public class AnyNumberTable {
-//this is commetented by Ganesh 
->>>>>>> a1825176017e5d312a31d2447a6a19d293f7bca9
-	public static void main(String[] args) {
+  	public static void main(String[] args) {
  		Scanner sc = new Scanner(System.in);
  		System.out.println("Enter any number!!!");
  		int num = sc.nextInt();
